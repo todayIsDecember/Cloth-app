63a589355de21544332570b10300458f15dc1904
todayIsDecember <timkivit@gmail.com>
2024-05-06 23:41:13 +0300

    fix bugs with images

--- app/components/MyImage/MyImage.tsx
+++ app/components/MyImage/MyImage.tsx
@@ -3,15 +3,32 @@
 import { MyImageProps } from "./MyImageProps";
 import cn from 'classnames';
 import styles from './MyImage.module.css';
-import Image from 'next/image'
+import NextImage from 'next/image'; // Перейменовано з Image на NextImage
 import { API } from "../../../helpers/api";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Modal } from "../modal/Modal";
 
 export const MyImage = ({images, className, ...props}: MyImageProps): JSX.Element => {
     const [isOpen, setIsOpen] = useState(false)
+    const [imageLoaded, setImageLoaded] = useState(false);
+
+    useEffect(() => {
+        const handleImageLoad = () => {
+            setImageLoaded(true);
+        };
+
+        // Ваш код для завантаження зображення
+        const imageElement = new Image();
+        imageElement.src = `${API.image.get}/${images[0]}`;
+        imageElement.onload = handleImageLoad;
+
+        // Прибираємо підписки при видаленні компонента
+        return () => {
+            imageElement.onload = null;
+        };
+    }, [images]);
 
     const openModal = () => {
         setIsOpen(true);
@@ -23,7 +40,7 @@ export const MyImage = ({images, className, ...props}: MyImageProps): JSX.Elemen
 
     return (
         <div className={cn(className, styles.imageContainer)} {...props}>
-            <Image src={`${API.image.get}/${images[0]}`} sizes="(100vw - 30px)" fill alt="image" onClick={openModal} priority className={styles.image}></Image>
+            <NextImage src={`${API.image.get}/${images[0]}`} fill alt="image" onClick={openModal} priority className={styles.image} />
             <Modal isOpen={isOpen} onClose={closeModal} images={images}></Modal>
         </div>
     )
